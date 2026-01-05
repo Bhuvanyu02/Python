54498b153731605974373e13c096825ebd8882ea
Bhuvanyu Guru <bhuvanyu0162.be20@chitkara.edu.in>
2026-01-05 22:04:21 +0530

    added madlibs_game.py (fill in the blanks with inputs)

---
+++ Python/madlibs_game.py
@@ -0,0 +1,13 @@
+# Word game to create a story by filling in blanks with random words
+
+adj1 = input("Adjective 1: ")
+noun1 = input("Noun: ")
+adj2 = input("Adjective 2: ")
+verb1 = input("Verb 1 (ending with 'ing'): ")
+adj3 = input("Adjective 3: ")
+
+print(f"Today I went to {adj1} zoo")
+print(f"In an exhibit I saw a {noun1}")
+print(f"{noun1} was a {adj2} and {verb1}")
+print(f"I was {adj3}!")
+
