efbe4ed17a85d5b98df9f591c8b63c77f1e29f6f
Bhuvanyu Guru <bhuvanyu0162.be20@chitkara.edu.in>
2026-01-07 20:20:06 +0530

    added maths functions in python

---
+++ Python/maths.py
@@ -0,0 +1,38 @@
+import math
+i=0
+
+i+=1
+# print(i)
+
+i-=1
+# print(i)
+
+i*=1
+# print(i)
+
+
+x = 3.14
+y = -4
+z = 5
+
+x=round(x)
+# print(x)
+
+y=abs(y)
+# print(y)
+
+z=pow(z,2)
+# print(z)
+
+# print(max(x,y,z))
+# print(min(x,y,z))
+
+# print(math.pi)
+# print(math.e)
+
+# print(math.sqrt(25)) # returns a float value
+
+# print(math.ceil(9.1)) # roundup
+
+# print(math.floor(9.1)) # round-down
+
