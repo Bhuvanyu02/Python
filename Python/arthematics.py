5ae7d3a7a0a66d9a628b30d40aac9bc97b37bf42
Bhuvanyu Guru <bhuvanyu0162.be20@chitkara.edu.in>
2026-01-14 15:16:49 +0530

    added maths functions in python

---
+++ Python/arthematics.py
@@ -0,0 +1,17 @@
+import math
+
+# radius = float(input(' Enter Radius: '))
+# circumference = 2 * math.pi* radius
+# print('Circumference = ',circumference)
+
+
+# radius = float(input(' Enter Radius: '))
+# area = math.pi * radius * radius
+# print('Area of circle: ',area)
+
+
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# c = round(math.sqrt(a*a+b*b),2)
+#
+# print('Hypotenuse is: ',c)
